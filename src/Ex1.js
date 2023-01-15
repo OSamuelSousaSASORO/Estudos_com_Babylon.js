9a6e89276feb23b8ba8119348d489130776da68a
OSamuelSousaSASORO <osamuelsousa@outlook.com>
2023-01-14 23:54:56 -0300

    Explorando os fundamentos do threejs (Aula 01)

--- src/Ex1.js
+++ src/Ex1.js
@@ -37,6 +37,26 @@ export default function Ex1() {
                 </mesh>
                 <CameraController/>
             </Canvas>
+
+            {/*
+            <Canvas> é Equvalente a const renderer = new THREE.WebGLRenderer({canvas}); 
+            camera propriedade é equivalente a const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);*/}
+            <Canvas>
+                {/*Usado para adicionar um laço entre os objetos que serão renderizados dentro dele */}
+                <mesh>
+                    {/*
+                    boxgeometry é equivalente a:
+                    const boxWidth = 1;
+                    const boxHeight = 1;
+                    const boxDepth = 1;
+                    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth); */}
+                    <boxGeometry args={[1,1,1]}></boxGeometry>
+                    {/*
+                    meshBasicMaterial é equivalente a:
+                    const material = new THREE.MeshBasicMaterial({color: 0x44aa88});*/}
+                    <meshBasicMaterial color='green'></meshBasicMaterial>
+                </mesh>
+            </Canvas>
         </>
     )
 }
